e02f599c9bcbd2c8cef97b4faba07e76b5ae3761
Nirjhari <nirjhari.jankar@gmail.com>
2018-05-31 16:04:54 +0530

    urls.py changed

--- docshare/urls.py
+++ docshare/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from docshareapp import views 
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
