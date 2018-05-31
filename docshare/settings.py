7d1703a5e97c9c346ba526bb601a19d69d0f8552
Nirjhari <nirjhari.jankar@gmail.com>
2018-05-31 15:42:33 +0530

    changes in settings

--- docshare/settings.py
+++ docshare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&*l()_4gv=ljccd$)&#5kfhw8x-8bd^rpt^kw=ghv$@lg-h7my'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
